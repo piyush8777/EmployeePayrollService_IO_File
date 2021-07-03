4c3004453a202fa4e9404590672d03cffe26ff66
Piyush Shaw <piyushshaw8777@gmail.com>
2021-07-04 02:59:59 +0530

    added :to watch particular directory with all files and sub directories

--- src/test/java/com/emppayroll/NIOFileAPITest.java
+++ src/test/java/com/emppayroll/NIOFileAPITest.java
@@ -10,57 +10,57 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class NIOFileAPITest {
-	private String HOME = System.getProperty("C:\\Users");
-    private String PLAY_WITH_NIO = "TempPlayGround";
+	 private String HOME = System.getProperty("user.home");
+	    private String PLAY_WITH_NIO = "TempPlayGround";
 
-    @Test
-    public void givenPathWhenCheckedThenConfirmed() throws IOException {
-        /**
-         * Check File Exist
-         */
+	    @Test
+	    public void givenPathWhenCheckedThenConfirmed() throws IOException {
+	        /**
+	         * Check File Exist
+	         */
 
-        Path homePath = Paths.get(HOME);
-        Assertions.assertTrue(Files.exists(homePath));
+	        Path homePath = Paths.get(HOME);
+	        Assertions.assertTrue(Files.exists(homePath));
 
-        /**
-         * Delete File and Check file is not exist
-         */
-        Path playpath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
-        if(Files.exists(playpath)) FileUtils.deleteFiles(playpath.toFile());
-        Assertions.assertTrue(Files.notExists(playpath));
+	        /**
+	         * Delete File and Check file is not exist
+	         */
+	        Path playpath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+	        if (Files.exists(playpath)) FileUtils.deleteFiles(playpath.toFile());
+	        Assertions.assertTrue(Files.notExists(playpath));
 
-        /**
-         *  Create Directory
-         */
-        Files.createDirectories(playpath);
-        Assertions.assertTrue(Files.exists(playpath));
+	        /**
+	         *  Create Directory
+	         */
+	        Files.createDirectories(playpath);
+	        Assertions.assertTrue(Files.exists(playpath));
 
-        /**
-         * create File
-         */
-        IntStream.range(1,10).forEach(cntr -> {
-            Path tempFile = Paths.get(playpath + ".temp"+cntr);
-            Assertions.assertTrue(Files.notExists(tempFile));
-            try{Files.createFile(tempFile);}
-            catch (IOException e){}
-            Assertions.assertTrue(Files.exists(tempFile));
-        });
+	        /**
+	         * create File
+	         */
+	        IntStream.range(1, 10).forEach(cntr -> {
+	            Path tempFile = Paths.get(playpath + ".temp" + cntr);
+	            Assertions.assertTrue(Files.notExists(tempFile));
+	            try{
+	                Files.createFile(tempFile);
+	            } catch (IOException e) {
+	            }
+	            Assertions.assertTrue(Files.exists(tempFile));
+	        });
 
-        /**
-         * List Files Directories as wll as File with Extension
-         */
-        Files.list(playpath).filter(Files::isRegularFile).forEach(System.out::println);
-        Files.newDirectoryStream(playpath).forEach(System.out::println);
-        Files.newDirectoryStream(playpath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
-                .forEach(System.out::println);
+	        /**
+	         * List Files Directories as wll as File with Extension
+	         */
+	        Files.list(playpath).filter(Files::isRegularFile).forEach(System.out::println);
+	        Files.newDirectoryStream(playpath).forEach(System.out::println);
+	        Files.newDirectoryStream(playpath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
+	                .forEach(System.out::println);
+	    }
 
+	    @Test
+	    public void givenADirecoryWhenWatchedListAllThheActivites()throws IOException{
+	        Path dir = Paths.get(HOME+ "/"+PLAY_WITH_NIO);
+	        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+	        new Java8WatchServiceExample(dir).processEvents();
+	    }
     }
-   
-
-@Test
-public void givenADirecoryWhenWatchedListAllThheActivites()throws IOException{
-    Path dir = Paths.get(HOME+ "/"+PLAY_WITH_NIO);
-    Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
-    new Java8WatchServiceExample(dir).processEvents();
-}
-}
